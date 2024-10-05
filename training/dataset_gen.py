0dde4bd97555dd3fb0d87d5ee5b5c7433ac31c45
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-10-05 04:48:00 -0400

    Dataset script

--- training/dataset_gen.py
+++ training/dataset_gen.py
@@ -1,7 +1,37 @@
-import pandas as pd 
-from sklearn.preprocessing import LabelEncoder
 
-accelerators = pd.read_excel('accelerators.xlsx')
-companies = pd.read_excel('companies.xlsx')
-products = pd.read_excel('products.xlsx')
-entitlements = pd.read_excel('entitlements.xlsx')
+import pandas as pd 
+import os
+
+
+accelerators = pd.read_csv('data/accelerators.csv')
+companies = pd.read_csv('data/companies.csv')
+products = pd.read_csv('data/products.csv')
+entitlements = pd.read_csv('data/entitlements.csv')
+
+
+train_data = []
+
+for _, company_row in companies.iterrows():
+    company_name = company_row['Name']
+    industry = company_row['Industry']
+    
+    company_entitlements = entitlements[entitlements['Company'] == company_name]
+    
+    for _, entitlement_row in company_entitlements.iterrows():
+        if entitlement_row['Implemented']:
+            product_name = entitlement_row['Product']
+            product_info = products[products['Name'] == product_name].iloc[0]
+            
+            train_data.append({
+                'company': company_name,
+                'industry': industry,
+                'product': {
+                    'ProductName': product_info['Name'],
+                    'Category': product_info['Category'],
+                    'IsImplemented': entitlement_row['Implemented']
+                }
+            })
+
+train_df = pd.DataFrame(train_data)
+
+print(train_df)
