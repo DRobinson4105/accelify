80ea3e393a327713de767fc7532435443a3f52f7
DRobinson4105 <drobinson4105@gmail.com>
2024-10-06 03:22:31 -0400

    Finished training loop and started flask endpoints

---
+++ api/server.py
@@ -0,0 +1,123 @@
+from utils import *
+import torch
+import pandas as pd
+from flask import Flask, request, jsonify, send_file
+from train import train
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# industry_map, product_name_map, product_category_map = label_to_idx()
+# industry_len, product_name_len, product_category_len = len(industry_map), len(product_name_map), len(product_category_map)
+
+# company_name = "Zen Zoology"
+
+# companies = pd.read_csv('data/companies.csv')
+# entitlements = pd.read_csv('data/entitlements.csv')
+# products_df = pd.read_csv('data/products.csv')
+
+# def get_industry_by_company_name(df, company_name):
+#     row = df[df['Name'] == company_name]
+#     if not row.empty:
+#         return row['Industry'].values[0]
+#     return None
+
+# def get_products_by_company_name(df, company_name):
+#     company_data = df[df['Company'] == company_name]
+
+#     return company_data['Product'].values, company_data['Implemented']
+
+# def get_category_by_name(df, name):
+#     row = df[df['Name'] == name]
+#     if not row.empty:
+#         return row['Category'].values[0]
+#     return None
+
+# industry = get_industry_by_company_name(companies, company_name)
+# names, implemented = get_products_by_company_name(entitlements, company_name)
+# names, implemented = names.tolist(), implemented.tolist()
+# categories = [get_category_by_name(products_df, name) for name in names]
+
+# product_name_len, product_category_len = len(product_name_map), len(product_category_map)
+
+# industry = torch.tensor(industry_map[industry]).to(device).view((1, 1))
+
+# product_names = torch.tensor([[product_name_map[y] for y in names]])
+# product_names = pad_sequence(product_names, batch_first=True, padding_value=product_name_len).to(device)
+
+# product_categories = torch.tensor([[product_category_map[y] for y in categories]])
+# product_categories = pad_sequence(product_categories, batch_first=True, padding_value=product_category_len).to(device)
+
+# product_is_implemented = torch.tensor([[y for y in implemented]]).long()
+# product_is_implemented = pad_sequence(product_is_implemented, batch_first=True, padding_value=2).to(device)
+
+# model = RecommenderModel(industry_len, product_name_len, product_category_len)
+# model.load_state_dict(torch.load('model.pth', weights_only=True))
+# model.eval()
+
+# with torch.inference_mode():
+#     output = model(industry, product_names, product_categories, product_is_implemented)
+
+# output_labels = list(product_name_map.keys())
+# output_labels = [x for _, x in sorted(zip(output.squeeze().cpu().numpy(), output_labels), reverse=True)]
+
+# print(f'Output: {output_labels}')
+
+app = Flask(__name__)
+
+THRESHOLD = 1
+
+def label_to_idx_maps():
+    #TODO mongodb retrieval data and build maps
+
+    return {}, {}, {}
+
+@app.route('/add_product', methods=['POST'])
+async def add_product():
+    #TODO mongodb retrieval dataset
+
+    product_name_map, product_category_map, industry_map = label_to_idx_maps()
+    train(product_name_map, product_category_map, industry_map)
+
+@app.route('/get_recommendations', methods=['POST'])
+async def get_recommendations():
+    data = request.get_json()
+    industry = data["industry"]
+    names = data["product_names"]
+    categories = data["product_categories"]
+    is_implemented = data["product_is_implemented"]
+
+    product_name_map, product_category_map, industry_map = label_to_idx_maps()
+    product_name_len, product_category_len, industry_len = len(product_name_map), len(product_category_map), len(industry_map)
+
+    industry = torch.tensor(industry_map[industry]).to(device).view((1, 1))
+
+    product_names = torch.tensor([[product_name_map[y] for y in names]])
+    product_names = pad_sequence(product_names, batch_first=True, padding_value=product_name_len).to(device)
+
+    product_categories = torch.tensor([[product_category_map[y] for y in categories]])
+    product_categories = pad_sequence(product_categories, batch_first=True, padding_value=product_category_len).to(device)
+
+    product_is_implemented = torch.tensor([[y for y in is_implemented]]).long()
+    product_is_implemented = pad_sequence(product_is_implemented, batch_first=True, padding_value=2).to(device)
+
+    model = RecommenderModel(industry_len, product_name_len, product_category_len)
+    model.load_state_dict(torch.load('model.pth', weights_only=True))
+    model.eval()
+
+    with torch.inference_mode():
+        output = model(industry, product_names, product_categories, product_is_implemented)
+
+    output_labels = list(product_name_map.keys())
+    
+    # TODO change threshold if needed
+    output_labels = [name for score, name in sorted(zip(output.squeeze().cpu().numpy(), output_labels), reverse=True) if score > THRESHOLD]
+
+    accelerators = []
+
+    for label in output_labels:
+        if label in names and is_implemented[names.index(label)]:
+            accelerators.append([label, 'TuneUp'])
+        else:
+            accelerators.append([label, 'JumpStart'])
+    
+    return jsonify({"accelerators": accelerators})
---
+++ api/utils.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_sequence, pad_sequence
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+import json
+
+class LSTMComponent(nn.Module):
+    def __init__(self, hidden_dim, output_dim):
+        super(LSTMComponent, self).__init__()
+        self.lstm = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, input):
+        packed_input = pack_sequence(input, enforce_sorted=False)
+        packed_output, (hidden, cell) = self.lstm(packed_input)
+        return self.fc(hidden.squeeze(0))
+
+class RecommenderModel(nn.Module):
+    def __init__(self, industry_size, product_name_size, product_category_size, hidden_dim=256, padding_value=-1):
+        super(RecommenderModel, self).__init__()
+
+        self.product_name_size = product_name_size
+        self.padding_value = padding_value
+
+        self.industry_embedding = nn.Embedding(num_embeddings=industry_size+1, embedding_dim=hidden_dim, padding_idx=industry_size)
+        self.product_name_embedding = nn.Embedding(num_embeddings=product_name_size+1, embedding_dim=hidden_dim, padding_idx=product_name_size)
+        self.product_category_embedding = nn.Embedding(num_embeddings=product_category_size+1, embedding_dim=hidden_dim, padding_idx=product_category_size)
+        self.product_is_implemented_embedding = nn.Embedding(num_embeddings=3, embedding_dim=hidden_dim, padding_idx=2)
+
+        self.lstm = LSTMComponent(hidden_dim=hidden_dim * 3, output_dim=hidden_dim)
+
+        self.fc1 = nn.Linear(hidden_dim * 2, 128)
+        self.fc2 = nn.Linear(128, product_name_size)
+
+        self.dropout = nn.Dropout(p=0.2)
+
+    def expand_product_name_size(self, new_size):
+        new_embedding = nn.Embedding(new_size, self.product_name_embedding.embedding_dim, padding_idx=self.padding_value)
+        new_fc = nn.Linear(32, new_size)
+        
+        new_embedding.weight.data[:self.product_name_embedding.num_embeddings] = self.product_name_embedding.weight.data
+        nn.init.normal_(new_embedding.weight.data[self.product_name_embedding.num_embeddings:])
+
+        with torch.no_grad():
+            new_fc.weight[:self.fc2.out_features] = self.fc2.weight
+            new_fc.bias[:self.fc2.out_features] = self.fc2.bias
+            nn.init.normal_(new_fc.weight[self.fc2.out_features:])
+            nn.init.normal_(new_fc.bias[self.fc2.out_features:])
+        
+        self.product_name_embedding = new_embedding
+        self.fc2 = new_fc
+
+        self.product_name_size = new_size
+
+    def forward(self, industry, product_name, product_category, product_is_implemented, new_size=None):
+        if new_size is not None and new_size > self.product_name_size:
+            self.expand_product_name_size(new_size)
+
+        industry_embedded = self.industry_embedding(industry)
+        product_name_embedded = self.product_name_embedding(product_name)
+        product_category_embedded = self.product_category_embedding(product_category)
+        product_is_implemented_embedded = self.product_is_implemented_embedding(product_is_implemented)
+
+        product_embedded = torch.cat([product_name_embedded, product_category_embedded, product_is_implemented_embedded], dim=-1)
+
+        product = self.lstm(product_embedded)
+
+        combined = torch.cat([industry_embedded.squeeze(1), product], dim=1)
+
+        x = F.relu(self.fc1(combined))
+        x = self.dropout(x)
+        output = self.fc2(x)
+
+        output = output[:, :self.product_name_size]
+
+        return output
+    
+class RecommenderDataset(Dataset):
+    def __init__(self, data, industry_map, product_name_map, product_category_map, device="cpu"):
+        product_name_len, product_category_len = len(product_name_map), len(product_category_map)
+        self.industries = torch.tensor([[industry_map[x["Input"]["Industry"]]] for x in data]).to(device)
+
+        self.product_names = [torch.tensor([product_name_map[y["ProductName"]] for y in x["Input"]["Products"]]) for x in data]
+        self.product_names = pad_sequence(self.product_names, batch_first=True, padding_value=product_name_len).to(device)
+
+        self.product_categories = [torch.tensor([product_category_map[y["ProductCategory"]] for y in x["Input"]["Products"]]) for x in data]
+        self.product_categories = pad_sequence(self.product_categories, batch_first=True, padding_value=product_category_len).to(device)
+
+        self.product_is_implemented = [torch.tensor([y["IsImplemented"] for y in x["Input"]["Products"]]).long() for x in data]
+        self.product_is_implemented = pad_sequence(self.product_is_implemented, batch_first=True, padding_value=2).to(device)
+
+        self.output = []
+
+        for x in data:
+            indices = torch.tensor([product_name_map[y] for y in x["Output"]["Names"]]).long()
+            values = torch.tensor(x["Output"]["Weights"]).float()
+
+            target = torch.zeros(product_name_len)
+
+            target[indices[indices < product_name_len]] = values
+
+            self.output.append(target)
+
+        self.output = pad_sequence(self.output, batch_first=True, padding_value=product_name_len).to(device)
+
+    def __len__(self):
+        return self.product_names.shape[0]
+    
+    def __getitem__(self, index):
+        return self.industries[index], self.product_names[index], self.product_categories[index], self.product_is_implemented[index], self.output[index]
+    
+def idx_to_label(file_path="labels.json"):
+    with open(file_path, "r") as file:
+        info = json.load(file)
+
+        product_name_map = {k: v for k, v in list(enumerate(info["Product_Names"]))}
+        product_category_map = {k: v for k, v in list(enumerate(info["Product_Categories"]))}
+        industry_map = {k: v for k, v in list(enumerate(info["Industries"]))}
+
+    return industry_map, product_name_map, product_category_map
+
+def label_to_idx(file_path="labels.json"):
+    with open(file_path, "r") as file:
+        info = json.load(file)
+
+        product_name_map = {k: v for v, k in list(enumerate(info["Product_Names"]))}
+        product_category_map = {k: v for v, k in list(enumerate(info["Product_Categories"]))}
+        industry_map = {k: v for v, k in list(enumerate(info["Industries"]))}
+
+    return industry_map, product_name_map, product_category_map
